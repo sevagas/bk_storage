687c3d6a0c987778f93be6449e973b118ed90106
Antoine Da Silva <antoine@balliskit.com>
2026-06-10 12:10:15 +0200

    initial de-upload container

--- BalliskitImplantContainers/Payload_Type/python_services/main.py
+++
@@ -1,7 +0,0 @@
-#from mywebhook.webhook import *
-import mythic_container
-import asyncio
-import balliskit_implant
-from translator.translator import *
-
-mythic_container.mythic_service.start_and_run_forever()
